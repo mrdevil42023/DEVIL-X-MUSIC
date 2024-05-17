b15aedc33c62e5a01d054b08172f4c1c830f0e8a
Mr Devil <168351611+mrdevil42023@users.noreply.github.com>
2024-05-17 17:00:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import re
+ import re
 import os
 from os import getenv
 from dotenv import load_dotenv
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/THE-VIP-BOY-OP/VIP-MUSIC",
+    "https://github.com/mrdevil42023/DEVIL-X-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
